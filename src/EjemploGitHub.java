04c9d4eefd4ffaeea9ffe33e14de9fd91bf5c9ec
leoal <leoal@LAPTOP-TDGJ8GOQ>
2020-05-13 00:30:05 -0600

    Primer proyecto

---
+++ src/EjemploGitHub.java
@@ -0,0 +1,14 @@
+//Ejemplo creado para subir a GitHub
+
+/**
+ *
+ * @author leoal
+ */
+public class EjemploGitHub {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
